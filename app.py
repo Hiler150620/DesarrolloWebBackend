3533f8a767d5ed49b83c6336ff0bf2d6ec94cf46
CarlosGarciaG <a01748393@tec.mx>
2022-04-07 21:52:56 -0500

    Revert "Update app.py"
    This reverts commit d4b431292b2945a50b6712866ba83948b479c473.

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-import email
-from flask import Flask, redirect, render_template, request, session, url_for
+
+from datetime import datetime
+
+from flask import Flask, render_template, request, session, redirect, url_for
 import datetime
+
+
 # FlASK
 #############################################################
 app = Flask(__name__)
 app.permanent_session_lifetime = datetime.timedelta(days=365)
 app.secret_key = "super secret key"
+
 #############################################################
 
 
@@ -19,17 +24,9 @@ def home():
         return render_template('login.html', data=email)
 
 
-@app.route('/signup')
-def signup():
-    name = request.form["name"]
-    email = request.form["email"]
-    password = request.form["password"]
-    return render_template('index.html')
-
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    email = None    
+    email = None
     if "email" in session:
         return render_template('index.html', data=session["email"])
     else:
@@ -40,3 +37,40 @@ def login():
             password = request.form["password"]
             session["email"] = email
             return render_template("index.html", data=email)
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if (request.method == "GET"):
+        return render_template("login.html", data="name")
+    else:
+        email = None
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        return render_template("index.html", data=name)
+
+
+@app.route("logout")
+def logout():
+    if "email" in session:
+        session.clear()
+        return redirect(url_for("home"))
+
+
+@app.route('/estructuradedatos')
+def prueba():
+    nombres = []
+    nombres.append({"nombre": "ruben",
+
+                    "Semetre01": [{
+                        "matematicas": "8",
+                        "español": "7"
+                    }],
+                    "Semetre02": [{
+                        "programacion": "5",
+                        "basededatos": "9"
+                    }]
+                    })
+
+    return render_template("home.html", data=nombres)
