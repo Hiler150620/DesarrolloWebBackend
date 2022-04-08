9cd58cdf74a82612353790f76458993ec3941330
CarlosGarciaG <a01748393@tec.mx>
2022-04-07 19:21:08 -0500

    login intento 1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from crypt import methods
+from flask import Flask, render_template, request
 
 # FlASK
 #############################################################
@@ -8,7 +9,27 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-    
-@app.route('/prueba')
+
+
+@app.route("/loginuser",methods=["POST"])
+def loginuser():
+    email = request.form["email"]
+    password = request.form["password"]
+    return render_template("login.html")
+
+@app.route('/estructuradedatos')
 def prueba():
-    return "Prueba"
+    nombres = []
+    nombres.append({"nombre": "ruben",
+
+                    "Semetre01": [{
+                        "matematicas": "8",
+                        "español": "7"
+                    }],
+                    "Semetre02": [{
+                        "programacion": "5",
+                        "basededatos": "9"
+                    }]
+                    })
+
+    return render_template("home.html", data=nombres)
